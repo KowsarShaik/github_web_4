b3d0f6a5c69763b25873f06e4377285cf1bb59b1
KowsarShaik <kowsarshaik419@gmail.com>
2021-11-11 13:21:56 +0530

    Upadated.java

--- HelloJava.java
+++ HelloJava.java
@@ -2,7 +2,7 @@ public class HelloJava {
 	
 	public static void main(String[] args) {
 
-		System.out.println("Welcome Java GitHub");
+		System.out.println("Welcome Java GitHub Desktop");
 		  
 		  
 	
